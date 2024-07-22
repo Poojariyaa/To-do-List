1fa02cf2073574b543c41af392febb3b022a6a46
Poojariyaa <poojavmay2004@gmail.com>
2024-07-22 20:18:43 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Todo-list/', // This is the base URL of your GitHub Pages site
+  base: '/todo-list/', // This is the base URL of your GitHub Pages site
 });
